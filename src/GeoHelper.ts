79f4646cdeb5c0587d1ffa7896df140926663679
viet nguyen <viet@noreply>
2018-11-29 00:47:20 -0800

    update API urls

--- src/GeoHelper.ts
+++ src/GeoHelper.ts
@@ -1,17 +1,15 @@
 import axios from "axios";
 import bbox from "@turf/bbox";
-import {FeatureCollection} from "geojson";
-
+import { FeatureCollection } from "geojson";
 
 export const getLatLngFromIP = async (): Promise<
   { lat: number; lng: number } | undefined
 > => {
   try {
     const response = await axios.get(
-      "http://ip-api.com/json"
+      "http://api.ipstack.com/check?access_key=0cd52c4d1a7b2e5edb3b17f203a8913e&format=2"
     );
-    console.log("### geoip data ", response.data);
-    return await { lat: response.data.lat, lng: response.data.lon };
+    return await { lat: response.data.latitude, lng: response.data.longitude };
   } catch (error) {
     console.error("getLatLngFromIP() error ", error);
     return undefined;
@@ -20,10 +18,15 @@ export const getLatLngFromIP = async (): Promise<
 
 export const bboxFlip = bbox => [[bbox[1], bbox[0]], [bbox[3], bbox[2]]];
 
-export const bboxFromGeoJson = (geojson: FeatureCollection): [number, number][] => {
-  if (geojson.features.length === 1 && geojson.features[0].geometry.type === "Point" ) {
+export const bboxFromGeoJson = (
+  geojson: FeatureCollection
+): [number, number][] => {
+  if (
+    geojson.features.length === 1 &&
+    geojson.features[0].geometry.type === "Point"
+  ) {
     const point = geojson.features[0].geometry.coordinates;
-    return [[point[1]-0.5, point[0]-0.5], [point[1]+0.5, point[0]+0.5]];
+    return [[point[1] - 0.5, point[0] - 0.5], [point[1] + 0.5, point[0] + 0.5]];
   }
   const _bbox = bbox(geojson);
   return [[_bbox[1], _bbox[0]], [_bbox[3], _bbox[2]]];
--- src/RestClient.ts
+++ src/RestClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {FeatureCollection} from "geojson";
 
 export const client = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "https://api.mappandas.com"
   /* other custom settings */
 });
 
